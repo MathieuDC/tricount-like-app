d00be853ca0a88003e900fbaa789cdc68074e5f3
mathieuDC <mdecampou@gmail.com>
2022-07-04 09:12:21 +0200

    [back] Creating Group

--- src/back/Group.tsx
+++ src/back/Group.tsx
@@ -4,15 +4,95 @@ interface Users{
   [index: number]: User;
 }
 
+interface Transaction{
+  id: number;
+  giverId: number;
+  receiverId: number;
+  amount: number;
+}
+
+interface Debt{
+  debterName: String;
+  creditorName: String;
+  amount: Number;
+}
+
+interface Transactions{
+  [index: number]: Transaction;
+}
+
 class Group{
   users: Users;
+  transactions: Transactions;
+  name: String;
+  debts: Debt[] = [];
 
-  constructor(){
+  constructor(name: String){
     this.users = {};
+    this.transactions = {};
+    this.name = name;
   }
 
   add(user: User){
     this.users[user.id] = user;
+  }
+
+  addTransaction(transaction: Transaction){
+    this.transactions[transaction.id] = transaction;
+    this.users[transaction.giverId].balance -= transaction.amount;
+    this.users[transaction.receiverId].balance += transaction.amount;
+  }
+
+  getUserWithMinBalance(users: Users){
+    let minBalance = 0;
+    let userId;
+    Object.keys(users).forEach(key => {
+      const user: User = users[Number(key)];
+      if(user.balance < minBalance){
+        minBalance = user.balance;
+        userId = user.id;
+      }
+    });
+    return userId;
+  }
+
+  computeDebt(){
+    const users: Users = JSON.parse(JSON.stringify(this.users));
+
+    Object.keys(users).forEach(key => {
+      const user: User = users[Number(key)];
+      if(user.balance > 0){
+        while(user.balance > 0){
+          let minBalanceId = this.getUserWithMinBalance(users);
+          const minBalanceUser = users[Number(minBalanceId)];
+          if(Math.abs(user.balance) >= Math.abs(minBalanceUser.balance)){
+            user.balance += minBalanceUser.balance;
+            let amount = Math.abs(minBalanceUser.balance);
+            minBalanceUser.balance = 0;
+            this.debts.push({debterName: user.firstName,creditorName: minBalanceUser.firstName, amount: amount})
+          } else {
+            minBalanceUser.balance += user.balance;
+            let amount = user.balance;
+            user.balance = 0;
+            this.debts.push({debterName: user.firstName,creditorName: minBalanceUser.firstName, amount: amount})
+          }
+        }
+      }
+    });
+  }
+
+  toString(){
+    let res = "Name  : " + this.name + "\n";
+    console.log(this.users)
+    Object.keys(this.users).forEach(key => {
+      const user: User = this.users[Number(key)];
+      res += user.name + " : " + String(user.balance) + "\n";
+    });
+    res += "\n"
+    this.debts.forEach(debt => {
+      res += debt.debterName + " doit " + debt.amount + " à " + debt.creditorName + "\n";
+    });
+    return res;
   }
 }
 
--- src/back/main.tsx
+++ src/back/main.tsx
@@ -1,9 +1,26 @@
+import User from "./User";
+import Group from "./Group";
+
 const db = {};
 
 function mainTest ()  {
-  console.log('oui');
+  const group = new Group("OM");
+
+  group.add(new User(1, "Messi", "Lionel"));
+  group.add(new User(2, "Suarez", "Luis"));
+  group.add(new User(3, "Payet", "Dimitry"));
+
+  group.addTransaction({id: 1, giverId: 1, receiverId: 2, amount: 10});
+  group.addTransaction({id: 2, giverId: 2, receiverId: 3, amount: 27});
+  group.addTransaction({id: 3, giverId: 2, receiverId: 1, amount: 3});
+  group.addTransaction({id: 4, giverId: 1, receiverId: 2, amount: 12});
+  group.addTransaction({id: 5, giverId: 3, receiverId: 2, amount: 24});
+  group.addTransaction({id: 6, giverId: 2, receiverId: 1, amount: 150});
+  group.addTransaction({id: 7, giverId: 1, receiverId: 3, amount: 60});
+
+  group.computeDebt();
+
+  console.log(group.toString());
 };
 
-mainTest();
-
-export {}
+export default mainTest;
