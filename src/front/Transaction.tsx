e524fa361597cedd43a03648ad3284d5fecfc487
mathieuDC <mdecampou@gmail.com>
2022-07-08 20:56:35 +0200

    Styling Transaction

--- src/front/Transaction.tsx
+++ src/front/Transaction.tsx
@@ -1,16 +1,18 @@
 import { FunctionComponent } from "react";
 import { TransactionI } from "../back/Transaction.class";
 
+import "./Transaction.css";
+
 interface TransactionProps {
   transaction: TransactionI
 }
  
 const Transaction = ({transaction}: TransactionProps) => {
   return ( 
-    <div>
-      <div>{transaction.name}</div>
-      <div>{transaction.amount}</div>
-      <div>{transaction.giver.name}</div>
+    <div className="transaction-item">
+      <div className="transaction-item__name">{transaction.name}</div>
+      <div className="transaction-item__amount">{transaction.amount}</div>
+      <div className="transaction-item__giver-name">{transaction.giver.name}</div>
     </div>
   );
 }
--- src/front/TransactionList.tsx
+++ src/front/TransactionList.tsx
@@ -1,13 +1,15 @@
 import { TransactionI, Transactions } from "../back/Transaction.class";
 import Transaction from "./Transaction";
 
+import "./Transaction.css"
+
 interface TransactionListProps {
   transactions: Transactions
 }
  
 export default ({transactions}: TransactionListProps) => {
   const transactionsItem = Object.values(transactions).map((t: TransactionI) => <Transaction transaction={t}></Transaction>)
-  return (<>
+  return (<ul className="transaction-list">
     {transactionsItem}
-  </>);
+  </ul>);
 };
