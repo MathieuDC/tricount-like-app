325f13fec00a52e88d5b53328adf1bb2ed0f9eba
mathieuDC <mdecampou@gmail.com>
2022-07-11 21:04:09 +0200

    Styling AddButton

--- src/front/AddButton.tsx
+++ src/front/AddButton.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { FormEventHandler } from "react";
 
+import "./AddButton.css"
+
 interface AddButtonProps {
   onChange: FormEventHandler;
 }
  
 const AddButton = ({onChange}: AddButtonProps) => {
-  return (<button className="add-button" onClick={onChange}>+++</button>  );
+  return (<button className="add-button" onClick={onChange}>+</button>  );
 }
  
 export default AddButton;
