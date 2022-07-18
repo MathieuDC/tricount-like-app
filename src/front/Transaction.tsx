51665eb5f7817cdb7817eff570972705912c962d
mathieuDC <mdecampou@gmail.com>
2022-07-18 19:34:04 +0200

    Adding date in Transanction

--- src/front/Transaction.tsx
+++ src/front/Transaction.tsx
@@ -9,11 +9,17 @@ interface TransactionProps {
 }
  
 const Transaction = ({transaction}: TransactionProps) => {
+
+  const padTo2Digits = (num: Number) => {
+    return num.toString().padStart(2, '0');
+  }
+
   return ( 
     <li key={transaction.id} className="transaction-item">
       <div className="transaction-item__name">{transaction.name}</div>
       <div className="transaction-item__amount">{transaction.amount}</div>
       <div className="transaction-item__giver-name">{transaction.giver.firstName}</div>
+      {transaction.date ? <div className="transaction-item__date">{padTo2Digits(transaction.date.getDate()) + "/" + padTo2Digits((transaction.date.getMonth() + 1)) + "/" + transaction.date.getFullYear ()}</div> : null}
     </li>
   );
 }
