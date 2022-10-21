b55671fdc6757d251b179ad4933eace03e9df03b
mathieuDC <mdecampou@gmail.com>
2022-10-21 09:42:58 +0200

    Add addTransaction feature

--- src/front/Group.tsx
+++ src/front/Group.tsx
@@ -1,31 +1,64 @@
 
 import React, { useEffect } from 'react';
 import { useState } from "react";
-import { IDebt, IGroup } from "../model/Group.class";
+import { IDebt, IGroup, Users } from "../../../back/src/models/Group";
 import Balances from "./Balances";
 import TransactionList from "./TransactionList";
 
 import "./Group.css"
 import NewTransaction from './NewTransaction';
+import { TransactionI } from '../../../back/src/models/Transaction';
 
 
 const Group = () => {
-
+  const [groupId, setGroupId] = useState('');
   const [groupName, setGroupName] = useState('');
   const [transactions, setTransactions] = useState({});
-  const [users, setUsers] = useState({});
+  const [users, setUsers] = useState<Users>({});
   const [debts, setDebts] = useState([] as IDebt[]);
   const [isBalancesDisplayed, setIsBalancesDisplayed] = useState(false);
   const [isDataFetched, setIsDataFetched] = useState(false);
 
   
   const updateState = (group: IGroup) => {
+    setGroupId(group.id);
     setTransactions({...group.transactions})
-    setUsers({...group.users})
+    setUsers(group.users)
     setDebts([...group.debts])
+  }
+
+  const fecthTransaction = async (transaction: TransactionI) => {
+    try {
+      const response = await fetch(`/groups/${groupId}/transaction`, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(transaction)
+      });
+      
+      const group = await response.json()
+      if(group){
+        return {
+          id: group.id,
+          transactions: group.transactions,
+          users: group.users,
+          name: group.name,
+          debts: group.debts
+        }
+       }
+    } catch (error) {
+      throw error;
+    }
   }
   
   const handleAddTransaction = (name: String, amount: number, giverId: number,receiverId: number) => {
+    fecthTransaction({name, amount, giver: users[giverId], receiver: users[receiverId]}).then(group => {
+      if(group)updateState(group)
+      else throw new Error("Didn't receive any group")
+    })
+    .catch(console.log)
     //groupBackEnd.addTransaction({name, amount, giver: groupBackEnd.users[giverId], receiver: groupBackEnd.users[receiverId]});
     //updateState();
   }
@@ -49,14 +82,7 @@ const Group = () => {
     try {
       const response = await fetch("/groups");
       const group = await response.json()
-      if(group){
-        return {
-          transactions: group.transactions,
-          users: group.users,
-          name: group.name,
-          debts: group.debts
-        }
-       }
+      if(group) return group
     } catch (error) {
       throw error;
     }
